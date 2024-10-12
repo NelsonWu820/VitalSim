adcfbfc887e6d3fa421bd89307db37f0e4f1c853
Nelson Wu <Nelsonwu820@gmail.com>
2024-10-12 15:37:55 -0400

    added a call to the fitbit api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const Scope = 'activity heartrate sleep';
 
+  const authFitBit = () => {
+    const fitBitUrl = `https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=${import.meta.env.VITE_FITBIT_CLIENT_ID}&redirect_uri=${encodeURIComponent(import.meta.env.VITE_FITBIT_REDIRECT_URI)}&scope=${Scope}&expires_in=604800`;
+
+    //sends user to fitbit's authorization page
+    window.location.href = fitBitUrl;
+  }
   return (
     <>
       <header>
         <h1>VitalSim</h1>
         <div>
-          <button>Allow Access to FitBit</button>
+          <button onClick={authFitBit}>Allow Access to FitBit</button>
         
         </div>
       </header>
