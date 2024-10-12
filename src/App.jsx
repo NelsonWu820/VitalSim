4d02b4f13212c5a7ec760281057132004f69251c
Nelson Wu <Nelsonwu820@gmail.com>
2024-10-12 17:24:31 -0400

    moved to multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import './App.css'
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AuthFitBit from './components/authFitBit';
+import FitbitRedirect from './components/redirectFitBit';
+import ResultFitBit from './components/ResultFitBit';
 
-function App() {
-  const Scope = 'activity heartrate sleep';
-
-  const authFitBit = () => {
-    const fitBitUrl = `https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=${import.meta.env.VITE_FITBIT_CLIENT_ID}&redirect_uri=${encodeURIComponent(import.meta.env.VITE_FITBIT_REDIRECT_URI)}&scope=${Scope}&expires_in=604800`;
-
-    //sends user to fitbit's authorization page
-    window.location.href = fitBitUrl;
-  }
+const App = () => {
   return (
-    <>
-      <header>
-        <h1>VitalSim</h1>
-        <div>
-          <button onClick={authFitBit}>Allow Access to FitBit</button>
-        
-        </div>
-      </header>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<AuthFitBit />} />
+        <Route path="/redirect" element={<FitbitRedirect />} />
+        <Route path="/result" element={<ResultFitBit />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
